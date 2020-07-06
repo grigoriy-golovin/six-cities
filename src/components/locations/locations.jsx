74ef0f6d52c6b1b087f543ac0b330bbed0ee5fb8
grigoriy-golovin <watson.180491@gmail.com>
2020-07-06 12:44:46 +0300

    сделал добавление активного класса городам при нажатии

--- src/components/locations/locations.jsx
+++ src/components/locations/locations.jsx
@@ -6,13 +6,13 @@ import {ActionCreator} from "./../../reducer.js";
 
 const Locations = (props) => {
 
-  const {cityArr, onChangecity} = props;
+  const {city, cityArr, onChangecity} = props;
 
   return <section className="locations container">
     <ul className="locations__list tabs__list">
       {cityArr.map((item) => {
         return <li className="locations__item" key={item}>
-          <a className="locations__item-link tabs__item" href="#" onClick={(evt) => onChangecity(item, evt)}>
+          <a className={`locations__item-link tabs__item ${item === city && `tabs__item--active`}`} href="#" onClick={(evt) => onChangecity(item, evt)}>
             <span>{item}</span>
           </a>
         </li>;
@@ -22,12 +22,13 @@ const Locations = (props) => {
 };
 
 Locations.propTypes = {
+  city: PropTypes.string.isRequired,
   cityArr: PropTypes.arrayOf(PropTypes.string).isRequired,
   onChangecity: PropTypes.func.isRequired
 };
 
 const mapStsteToProps = (state, ownProps) => Object.assign({}, ownProps, {
-  // city: state.city,
+  city: state.city,
   cityArr: Array.from(new Set(state.offers.map((item) => item.city.name)))
 
 });
@@ -35,6 +36,8 @@ const mapStsteToProps = (state, ownProps) => Object.assign({}, ownProps, {
 const mapDispatchToProps = (dispatch) => ({
   onChangecity: (cityName, evt) => {
     evt.preventDefault();
+    document.querySelector(`.tabs__item--active`).classList.remove(`tabs__item--active`);
+    evt.currentTarget.classList.add(`tabs__item--active`);
     dispatch(ActionCreator.changeCity(cityName));
     dispatch(ActionCreator.setOffersForSity());
   },
