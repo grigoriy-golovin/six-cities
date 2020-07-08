735e23c4f559dd6b13595fa231e7c2634b00c9b7
grigoriy-golovin <watson.180491@gmail.com>
2020-07-08 15:04:45 +0300

    добавил выподание меню сортировки

--- src/components/main/places-sotring/places-sotring.jsx
+++ src/components/main/places-sotring/places-sotring.jsx
@@ -2,22 +2,37 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 // import {connect} from 'react-redux';
 
-const PlacesSorting = () => {
-  return <form className="places__sorting" action="#" method="get">
-    <span className="places__sorting-caption">Sort by</span>
-    <span className="places__sorting-type" tabIndex="0">
-      Popular
-      <svg className="places__sorting-arrow" width="7" height="4">
-        <use xlinkHref="#icon-arrow-select"></use>
-      </svg>
-    </span>
-    <ul className="places__options places__options--custom places__options--opened">
-      <li className="places__option places__option--active" tabIndex="0">Popular</li>
-      <li className="places__option" tabIndex="0">Price: low to high</li>
-      <li className="places__option" tabIndex="0">Price: high to low</li>
-      <li className="places__option" tabIndex="0">Top rated first</li>
-    </ul>
-  </form>;
-};
+class PlacesSorting extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // isOpened: false,
+      activeOptions: `Popular`
+    };
+  }
+
+  render() {
+    return <form className="places__sorting" action="#" method="get">
+      <span className="places__sorting-caption">Sort by </span>
+      <span className="places__sorting-type" tabIndex="0" onClick={this.handlerSortType}>
+        {this.state.activeOptions}
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use xlinkHref="#icon-arrow-select"></use>
+        </svg>
+      </span>
+      <ul className="places__options places__options--custom">
+        <li className="places__option places__option--active" tabIndex="0">Popular</li>
+        <li className="places__option" tabIndex="0">Price: low to high</li>
+        <li className="places__option" tabIndex="0">Price: high to low</li>
+        <li className="places__option" tabIndex="0">Top rated first</li>
+      </ul>
+    </form>;
+  }
+
+  handlerSortType() {
+    document.querySelector(`.places__options`).classList.toggle(`places__options--opened`);
+  }
+
+}
 
 export default PlacesSorting;
