d05c5c2b83d6c436846a6dc990921f515247cf08
grigoriy-golovin <watson.180491@gmail.com>
2020-07-28 08:08:19 +0300

    сделано отображение пустой страницы избранного по макету

--- src/components/favorites/favorites.jsx
+++ src/components/favorites/favorites.jsx
@@ -9,31 +9,45 @@ const Favorites = (props) => {
   const {offers} = props;
   const cityArr = Array.from(new Set(offers.map((item) => item.city.name)));
   return <Fragment>
-    <main className="page__main page__main--favorites">
-      <div className="page__favorites-container container">
-        <section className="favorites">
-          <h1 className="favorites__title">Saved listing</h1>
-          <ul className="favorites__list">
-            {cityArr.map((cityName) => {
-              return <li className="favorites__locations-items" key={cityName}>
-                <div className="favorites__locations locations locations--current">
-                  <div className="locations__item">
-                    <a className="locations__item-link" href="#">
-                      <span>{cityName}</span>
-                    </a>
+    {offers.length === 0 ?
+      <main className="page__main page__main--favorites page__main--favorites-empty">
+        <div className="page__favorites-container container">
+          <section className="favorites favorites--empty">
+            <h1 className="visually-hidden">Favorites (empty)</h1>
+            <div className="favorites__status-wrapper">
+              <b className="favorites__status">Nothing yet saved.</b>
+              <p className="favorites__status-description">Save properties to narrow down search or plan yor future trips.</p>
+            </div>
+          </section>
+        </div>
+      </main>
+      :
+      <main className="page__main page__main--favorites">
+        <div className="page__favorites-container container">
+          <section className="favorites">
+            <h1 className="favorites__title">Saved listing</h1>
+            <ul className="favorites__list">
+              {cityArr.map((cityName) => {
+                return <li className="favorites__locations-items" key={cityName}>
+                  <div className="favorites__locations locations locations--current">
+                    <div className="locations__item">
+                      <a className="locations__item-link" href="#">
+                        <span>{cityName}</span>
+                      </a>
+                    </div>
                   </div>
-                </div>
-                <div className="favorites__places">
-                  {offers.filter((offer) => offer.city.name === cityName).map((item) => {
-                    return <FavoriteCard offer={item} key={item.id}/>;
-                  })}
-                </div>
-              </li>;
-            })}
-          </ul>
-        </section>
-      </div>
-    </main>
+                  <div className="favorites__places">
+                    {offers.filter((offer) => offer.city.name === cityName).map((item) => {
+                      return <FavoriteCard offer={item} key={item.id}/>;
+                    })}
+                  </div>
+                </li>;
+              })}
+            </ul>
+          </section>
+        </div>
+      </main>
+    }
     <footer className="footer container">
       <Link className="footer__logo-link" to={`/`}>
         <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
