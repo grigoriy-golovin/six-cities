6f175b136f2ed2cb805976ab25311d568db2f02d
grigoriy-golovin <watson.180491@gmail.com>
2020-07-30 12:52:31 +0300

    заменил путь в футоре favorites

--- src/components/favorites/favorites.jsx
+++ src/components/favorites/favorites.jsx
@@ -50,7 +50,7 @@ const Favorites = (props) => {
     }
     <footer className="footer container">
       <Link className="footer__logo-link" to={`/`}>
-        <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
+        <img className="footer__logo" src="/six-cities/public/img/logo.svg" alt="6 cities logo" width="64" height="33" />
       </Link>
     </footer>
   </Fragment>;
